566894a99db96e187d66cb4b648f92589a17816f
Fabian Richter <fabian.richter.97@gmail.com>
2016-12-22 23:26:42 +0100

    fixed stuff via dev versions

--- classification/GitHelper.py
+++ classification/GitHelper.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 from github3 import login
+from github3.null import NullObject
 from markdown import markdown
 from bs4 import BeautifulSoup
 from random import randint
 
 def _token():
-    tokens = ['9e484681cd48b198297bb0de032445f92a962282', '360e0d54fe6c4e1e944bcb6c2ed0533389683758']
+    tokens = ['9e484681cd48b198297bb0de032445f92a962282',
+              '360e0d54fe6c4e1e944bcb6c2ed0533389683758']
     i = randint(0, len(tokens) - 1)
     return tokens[i]
+
 
 class Git(object):
     """docstring for Git."""
@@ -17,35 +20,34 @@ class Git(object):
         super(Git, self).__init__()
         self.api = login(token=_token())
         # repo may not exist
-        self.repo = None
-        try:
-            self.repo = self._get_repo(user, title)
-            self.valid = True
-        except StopIteration:
-            self.valid = False
-
-    def _get_repo(self, user, title):
-        return self.api.search_repositories('%s user:%s fork:true' % (title, user)).next().repository
+        self.repo = self.api.repository(user, title)
+        self.valid = not type(self.repo) == NullObject
 
     def number_contributors(self):
-        return len(list(self.repo.iter_contributors()))
+        return len(list(self.repo.contributors()))
 
     def get_readme(self):
         readme = self.repo.readme()
-        if(readme is not None):
+        if not type(readme) == NullObject:
             text = BeautifulSoup(readme.decoded, "html.parser").text
             return text
         return ''
 
     def get_commits(self):
-        repo = list(self.repo.iter_commits())
-        return list(map(lambda x: x.commit.message, repo))
+        commits = list(self.repo.commits())
+        return list(map(lambda x: x.message, commits))
 
     def get_issues(self):
-        repo = list(self.repo.iter_issues())
-        return list(map(lambda x: list(map(lambda y: y.body_text
-                                           , x.iter_comments()))
-                        , repo))
+        issues = list(self.repo.issues())
+        titles = list(map(lambda issue: issue.title, issues))
+        comments = list(map(lambda issue: list(map(
+            lambda comment: comment.body_text, issue.comments())),
+            issues))
+
+        i = 0
+        for i in range(len(titles)):
+            comments[i].insert(0, titles[i])
+        return comments
 
     def get_times(self):
         created = self.repo.created_at.timestamp()

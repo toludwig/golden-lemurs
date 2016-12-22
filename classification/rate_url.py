0649431420ba88c06a1268263671f3b40d646c5a
Maxim Schuwalow <mschuwalow@uos.de>
2016-12-22 15:06:06 +0100

    corrected mistakes

--- classification/rate_url.py
+++ classification/rate_url.py
@@ -5,12 +5,14 @@ import clipboard
 from optparse import OptionParser
 from .GitHelper import Git
 
+
 def _options():
     parser = OptionParser()
     parser.add_option("-o", "--output", dest="out", action="store",
                       type="string", default='./results.json', help="file to write to")
 
     return parser.parse_args()
+
 
 def _split_url(url):
     """
@@ -23,25 +25,28 @@ def _split_url(url):
     user = split[3]
     return (user, title)
 
+
 def download_fields(url):
     user, title = _split_url(url)
     git = Git(user, title)
     obj = {}
     obj["URL"] = url
-    obj["User"]                 = user
-    obj["Title"]                = title
-    obj["Readme"]               = git.get_readme()
+    obj["User"] = user
+    obj["Title"] = title
+    obj["Readme"] = git.get_readme()
     obj["NumberOfContributors"] = git.number_contributors()
-    obj["NumberCommits"]        = git.number_commits()
-    obj["Commits"]              = git.get_commits()
-    obj["NumberIssues"]         = git.number_issues()
-    obj["Issues"]               = git.get_issues()
-    obj["Times"]                = git.get_times()
+    obj["NumberCommits"] = git.number_commits()
+    obj["Commits"] = git.get_commits()
+    obj["NumberIssues"] = git.number_issues()
+    obj["Issues"] = git.get_issues()
+    obj["Times"] = git.get_times()
     return obj
+
 
 def _save(data, file):
     with open(file, "w") as f:
         json.dump(data, f, sort_keys=True, indent=4 * " ")
+
 
 def main():
     (options, args) = _options()
@@ -56,7 +61,8 @@ def main():
 
         trying = True
         while trying:
-            c = input("Ratings: [1] DEV [2] HW [3] EDU [4] DOCS [5] WEB [6] DATA [7] OTHER [S]kip [Q]uit\n")
+            c = input(
+                "Ratings: [1] DEV [2] HW [3] EDU [4] DOCS [5] WEB [6] DATA [7] OTHER [S]kip [Q]uit\n")
             if c in ['q', 'Q']:
                 _save(results, options.out)
                 return

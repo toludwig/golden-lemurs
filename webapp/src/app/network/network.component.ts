5e07a2d745d408f68db0cdf2798133d7cfc41ed0
Fabian Richter <fabian.richter.97@gmail.com>
2017-01-07 13:11:24 +0100

    visualization stuff

--- webapp/src/app/network/network.component.ts
+++ webapp/src/app/network/network.component.ts
@@ -20,7 +20,6 @@ export interface Neuron {
   output: number;
 }
 
-
 @Component({
   selector: 'app-network',
   templateUrl: './network.component.html',
@@ -93,109 +92,247 @@ export class NetworkComponent implements AfterViewInit {
   constructor() { }
 
   ngAfterViewInit() {
+
+    var w = window.innerWidth * 0.68 * 0.95;
+    var h = Math.ceil(w * 0.7);
+    var oR = 0;
+    var nTop = 0;
+
     let elem = this.net.nativeElement;
 
-    function sum(list: Array<number>) {
-      return list.reduce((last, curr) => last + curr, 0);
-    }
-    function cumsum(list: Array<number>, index) {
-      if (index == 0) return 0;
-      return list.reduce((last, curr, i) => (i < index ? last + curr : last), 0);
-    }
+    var svg = d3.select(elem).append("svg")
+      .attr("class", "mainBubbleSVG")
+      .attr("width", w)
+      .attr("height", h)
+      .on("mouseleave", function() { return resetBubbles(); });
 
-    function size(obj, level: number, dimension: 'x' | 'y'): number {
-      switch (level) {
-        case 0: // neuron
-          return 10;
-        case 1: // layer
-          if (dimension == 'x')
-            return sum((obj as Layer).neurons.map(n => size(n, level - 1, dimension))) * 1.1;
-          else
-            return 30;
-        case 2: // network
-          if (dimension == 'y')
-            return Math.max(...(obj as Network).layers.map(n => size(n, level - 1, dimension))) * 1.1 + 20;
-          else
-            return sum((obj as Network).layers.map(n => size(n, level - 1, dimension))) * 1.1 + 10;
+    var mainNote = svg.append("text")
+      .attr("id", "bubbleItemNote")
+      .attr("x", 10)
+      .attr("y", w / 2 - 15)
+      .attr("font-size", 12)
+      .attr("dominant-baseline", "middle")
+      .attr("alignment-baseline", "middle")
+      .style("fill", "#888888")
+      .text(function(d) { return "D3.js bubble menu developed by Shipeng Sun (sunsp.gis@gmail.com), Institute of Environment, University of Minnesota, and University of Springfield, Illinois."; });
+
+
+    d3.json("/assets/network.json", function(error, root: any) {
+      console.log(error);
+
+      var bubbleObj = svg.selectAll(".topBubble")
+        .data(root.children)
+        .enter().append("g")
+        .attr("id", function(d, i) { return "topBubbleAndText_" + i });
+
+      console.log(root);
+      nTop = root.children.length;
+      oR = w / (1 + 3 * nTop);
+
+      h = Math.ceil(w / nTop * 2);
+      //svgContainer.style("height", h + "px");
+
+      var colVals = d3.scaleOrdinal(d3.schemeCategory10);
+
+      bubbleObj.append("circle")
+        .attr("class", "topBubble")
+        .attr("id", function(d, i) { return "topBubble" + i; })
+        .attr("r", function(d) { return oR; })
+        .attr("cx", function(d, i) { return oR * (3 * (1 + i) - 1); })
+        .attr("cy", (h + oR) / 3)
+        .style("fill", function(d, i: any) { return colVals(i); }) // #1f77b4
+        .style("opacity", 0.3)
+        .on("mouseover", function(d, i) { return activateBubble(d, i); });
+
+
+      bubbleObj.append("text")
+        .attr("class", "topBubbleText")
+        .attr("x", function(d, i) { return oR * (3 * (1 + i) - 1); })
+        .attr("y", (h + oR) / 3)
+        .style("fill", function(d, i: any) { return colVals(i); }) // #1f77b4
+        .attr("font-size", 30)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "middle")
+        .attr("alignment-baseline", "middle")
+        .text(function(d: any) { return d.name })
+        .on("mouseover", function(d, i) { return activateBubble(d, i); });
+
+
+      for (var iB = 0; iB < nTop; iB++) {
+        var childBubbles = svg.selectAll(".childBubble" + iB)
+          .data(root.children[iB].children)
+          .enter().append("g");
+
+        //var nSubBubble = Math.floor(root.children[iB].children.length/2.0);
+
+        childBubbles.append("circle")
+          .attr("class", "childBubble" + iB)
+          .attr("id", function(d, i) { return "childBubble_" + iB + "sub_" + i; })
+          .attr("r", function(d) { return oR / 3.0; })
+          .attr("cx", function(d, i) { return (oR * (3 * (iB + 1) - 1) + oR * 1.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("cy", function(d, i) { return ((h + oR) / 3 + oR * 1.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("cursor", "pointer")
+          .style("opacity", 0.5)
+          .style("fill", "#eee")
+          .on("click", function(d: any, i) {
+            window.open(d.address);
+          })
+          .on("mouseover", function(d: any, i) {
+            //window.alert("say something");
+            var noteText = "";
+            if (d.note == null || d.note == "") {
+              noteText = d.address;
+            } else {
+              noteText = d.note;
+            }
+            d3.select("#bubbleItemNote").text(noteText);
+          })
+          .append("svg:title")
+          .text(function(d: any) { return d.address; });
+
+        childBubbles.append("text")
+          .attr("class", "childBubbleText" + iB)
+          .attr("x", function(d, i) { return (oR * (3 * (iB + 1) - 1) + oR * 1.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("y", function(d, i) { return ((h + oR) / 3 + oR * 1.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); })
+          .style("opacity", 0.5)
+          .attr("text-anchor", "middle")
+          .style("fill", function(d, i) { return colVals(iB as any); }) // #1f77b4
+          .attr("font-size", 6)
+          .attr("cursor", "pointer")
+          .attr("dominant-baseline", "middle")
+          .attr("alignment-baseline", "middle")
+          .text(function(d: any) { return d.name })
+          .on("click", function(d: any) {
+            window.open(d.address);
+          });
+
+      }
+
+
+    });
+
+    function resetBubbles() {
+      w = window.innerWidth * 0.68 * 0.95;
+      oR = w / (1 + 3 * nTop);
+
+      h = Math.ceil(w / nTop * 2);
+      //svgContainer.style("height", h + "px");
+
+      mainNote.attr("y", h - 15);
+
+      svg.attr("width", w);
+      svg.attr("height", h);
+
+      d3.select("#bubbleItemNote").text("D3.js bubble menu developed by Shipeng Sun (sunsp.gis@gmail.com), Institute of Environment, University of Minnesota, and University of Springfield, Illinois.");
+
+      var t = svg.transition()
+        .duration(650);
+
+      t.selectAll(".topBubble")
+        .attr("r", function(d) { return oR; })
+        .attr("cx", function(d, i) { return oR * (3 * (1 + i) - 1); })
+        .attr("cy", (h + oR) / 3);
+
+      t.selectAll(".topBubbleText")
+        .attr("font-size", 30)
+        .attr("x", function(d, i) { return oR * (3 * (1 + i) - 1); })
+        .attr("y", (h + oR) / 3);
+
+      for (var k = 0; k < nTop; k++) {
+        t.selectAll(".childBubbleText" + k)
+          .attr("x", function(d, i) { return (oR * (3 * (k + 1) - 1) + oR * 1.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("y", function(d, i) { return ((h + oR) / 3 + oR * 1.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("font-size", 6)
+          .style("opacity", 0.5);
+
+        t.selectAll(".childBubble" + k)
+          .attr("r", function(d) { return oR / 3.0; })
+          .style("opacity", 0.5)
+          .attr("cx", function(d, i) { return (oR * (3 * (k + 1) - 1) + oR * 1.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("cy", function(d, i) { return ((h + oR) / 3 + oR * 1.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); });
+
       }
     }
 
-    const netXSize = this.architecture.map(n => size(n, 2, 'x'));
-    const netYSize = this.architecture.map(n => size(n, 2, 'y'));
-    const netX = netXSize.map((_, i) => 0); //cumsum(netXSize, i));
-    const netY = netYSize.map((_, i) => cumsum(netYSize, i));
 
-    let svg = d3.select(elem)
-      .attr('width', 1000)
-      .attr('height', 600);
-    let g = svg.append('g');
-    svg.call(d3.zoom().scaleExtent([1 / 8, 16])
-      .on('zoom', function() { g.attr('transform', d3.event.transform); }));
-    let networks = g.selectAll('g.network').data(this.architecture);
-    networks = networks.enter().append('g')
-      .classed('network', true)
-      .merge(networks)
-      .attr('transform', (network, i) => {
-        return `translate(${netX[i]}, ${netY[i]})`;
-      });
-    networks
-      .append('text')
-      .attr('x', 0)
-      .attr('y', -10)
-      .attr('font-size', 10)
-      .text(network => network.name);
-    networks
-      .append('text')
-      .attr('font-size', 10)
-      .attr('x', -20)
-      .attr('y', 15)
-      .text(network => network.input);
-    networks
-      .append('text')
-      .attr('x', n => size(n, 2, 'x'))
-      .attr('y', 15)
-      .attr('font-size', 10)
-      .text(network => network.output);
+    function activateBubble(d, i) {
+      // increase this bubble and decrease others
+      var t = svg.transition()
+        .duration(d3.event.altKey ? 7500 : 350);
 
-    let myLayers = d3.local();
-    let layers = networks.selectAll('g.layer').data(function (network) {
-      myLayers.set(this as Element, network.layers.map(l => size(l, 1, 'x')));
-      return network.layers;
-    });
-    layers = layers.enter().append('g')
-      .classed('layer', true)
-      .merge(layers)
-      .attr('transform', function (layer, i) {
-        let layers = myLayers.get(this as Element) as number[];
-         return `translate(${cumsum(layers, i) * 1.2 }, 0)`;
-    } );
-    layers
-      .append('rect')
-      .attr('width', l => size(l, 1, 'x'))
-      .attr('height', l => size(l, 1, 'y'))
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('fill-opacity', '0')
-      .attr('stroke-width', '2')
-      .attr('stroke', 'black');
+      t.selectAll(".topBubble")
+        .attr("cx", function(d, ii) {
+          if (i == ii) {
+            // Nothing to change
+            return oR * (3 * (1 + ii) - 1) - 0.6 * oR * (ii - 1);
+          } else {
+            // Push away a little bit
+            if (ii < i) {
+              // left side
+              return oR * 0.6 * (3 * (1 + ii) - 1);
+            } else {
+              // right side
+              return oR * (nTop * 3 + 1) - oR * 0.6 * (3 * (nTop - ii) - 1);
+            }
+          }
+        })
+        .attr("r", function(d, ii) {
+          if (i == ii)
+            return oR * 1.8;
+          else
+            return oR * 0.8;
+        });
 
-    let neurons = layers.selectAll('circle.neuron').data(layer => layer.neurons);
-    neurons.enter().append('circle')
-      .classed('neuron', true)
-      .merge(neurons)
-      .attr('cx', d => size(d, 0, 'x') * 1.2)
-      .attr('cy', (d, i) => (i + 1) * size(d, 0, 'x') * 1.1)
-      .attr('r', n => size(n, 0, 'x') / 2)
-      .attr('fill-opacity', '0')
-      .attr('stroke-width', '1')
-      .attr('stroke', 'black')
-    let weights = layers.selectAll('text.neuron').data(layer => layer.neurons);
-    weights.enter().append('text')
-      .text(neuron => `${neuron.weight}`)
-      .attr('font-size', d => size(d, 0, 'x') * 0.5)
-      .attr('text-anchor', 'middle')
-      .attr('x', d => size(d, 0, 'x') * 1.2)
-      .attr('y', (d, i) => (i + 1) * size(d, 0, 'x') * 1.2);
+      t.selectAll(".topBubbleText")
+        .attr("x", function(d, ii) {
+          if (i == ii) {
+            // Nothing to change
+            return oR * (3 * (1 + ii) - 1) - 0.6 * oR * (ii - 1);
+          } else {
+            // Push away a little bit
+            if (ii < i) {
+              // left side
+              return oR * 0.6 * (3 * (1 + ii) - 1);
+            } else {
+              // right side
+              return oR * (nTop * 3 + 1) - oR * 0.6 * (3 * (nTop - ii) - 1);
+            }
+          }
+        })
+        .attr("font-size", function(d, ii) {
+          if (i == ii)
+            return 30 * 1.5;
+          else
+            return 30 * 0.6;
+        });
+
+      var signSide = -1;
+      for (var k = 0; k < nTop; k++) {
+        signSide = 1;
+        if (k < nTop / 2) signSide = 1;
+        t.selectAll(".childBubbleText" + k)
+          .attr("x", function(d, i) { return (oR * (3 * (k + 1) - 1) - 0.6 * oR * (k - 1) + signSide * oR * 2.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("y", function(d, i) { return ((h + oR) / 3 + signSide * oR * 2.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("font-size", function() {
+            return (k == i) ? 12 : 6;
+          })
+          .style("opacity", function() {
+            return (k == i) ? 1 : 0;
+          });
+
+        t.selectAll(".childBubble" + k)
+          .attr("cx", function(d, i) { return (oR * (3 * (k + 1) - 1) - 0.6 * oR * (k - 1) + signSide * oR * 2.5 * Math.cos((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("cy", function(d, i) { return ((h + oR) / 3 + signSide * oR * 2.5 * Math.sin((i - 1) * 45 / 180 * 3.1415926)); })
+          .attr("r", function() {
+            return (k == i) ? (oR * 0.55) : (oR / 3.0);
+          })
+          .style("opacity", function() {
+            return (k == i) ? 1 : 0;
+          });
+      }
+    }
+
+    window.onresize = resetBubbles;
   }
 
 }

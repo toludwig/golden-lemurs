0825416a2615d6e3c89dee01088ef9a551684add
Maxim Schuwalow <Mschuwalow@uos.de>
2017-01-19 10:33:47 +0100

    followup. more sophisticated. call with ./tensorboard.sh ../out

---
+++ scripts/tensorboard.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+dir=$1
+
+if [[ ! -d $dir ]]; then
+    echo "$dir is not a dirhectory..."
+    exit 1
+fi
+
+cd $dir
+
+logdirs=""
+for entry in $(ls); do
+    if [[ -d $entry && -d $entry/summary ]]; then
+        logdirs="$logdirs$entry:$entry/summary,"
+    fi
+done
+
+if [[ $logdirs -eq "" ]]; then
+    echo "No summary dirs found in subfolders of $dir."
+    exit 1
+fi
+
+tensorboard --logdir=$logdirs --reload_interval 30
+exit 0
